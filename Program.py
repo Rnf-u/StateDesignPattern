1d9069eda3c134f582bdf19f7b83ac0fe0960b23
Arriane Yu <arrianeyu08@gmail.com>
2022-10-25 22:44:41 +0800

    Added legit code for timer

--- Program.py
+++ Program.py
@@ -11,6 +11,8 @@ choice = int(input("Do you want to turn on the traffic light? (1=YES, 0=NO): "))
 
 if choice == 1:
     toggleTrafficLight(light)
+    light.state.timer()
+    toggleTrafficLight(light)
 else:
     print("CURRENT STATE: ")
     light.state.doAction()
--- State.py
+++ State.py
@@ -1,4 +1,5 @@
 from Light import *
+from time import sleep
 
 class ITrafficState:
     def doAction(self):
@@ -6,7 +7,14 @@ class ITrafficState:
 
 class OnTrafficState(ITrafficState):
     def __init__(self):
-        self.states = ["STOP", "GO", "SLOW DOWN"]
+        self.states = {"STOP": 10, "GO": 20, "SLOW DOWN": 5}
+
+    def timer(self):
+        for x, y in self.states.items():
+            print("------", x, "-------")
+            for i in range(y, 0, -1):
+                print("%d..." % i)
+                sleep(1)
 
     def doAction(self):
         print("TRAFFIC LIGHT ON!")
